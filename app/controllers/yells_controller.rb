a19203d01a575a398dd306c00ac5f6f28e807242
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-11-17 16:02:02 +0900

    Fix controller

--- app/controllers/yells_controller.rb
+++ app/controllers/yells_controller.rb
@@ -1,5 +1,5 @@
 class YellsController < ApplicationController
-  before_filter :setup
+  before_filter :setup, only: [:show]
   def index
   end
 
@@ -8,6 +8,8 @@ class YellsController < ApplicationController
 
   private
   def setup
-    @yell = Yell.find_by_name(params[:id]) || Yell.find(params[:id])
+    if params[:id]
+      @yell = Yell.find_by_name(params[:id]) || Yell.find(params[:id])
+    end
   end
 end
