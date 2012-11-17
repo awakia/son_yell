5e03f5d90f5978149a97377d82d685cc206b8f57
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-11-17 10:19:11 +0900

    Initial commit (rails new son_yell --skip-test-unit)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SonYell::Application.config.secret_token = '3aa0a69a9a9136b57890032aa7d62dafa23ea519ef38626f183888170ea821572d631e5728536ec6e6e21d822284e80f1d32103caf917b2cb53165b2f62242bb'
