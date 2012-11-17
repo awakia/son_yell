061b2f5c519cfad9f63ccbce9643704ed52edf9d
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-11-17 15:49:26 +0900

    Add show page

--- app/controllers/yells_controller.rb
+++ app/controllers/yells_controller.rb
@@ -1,7 +1,13 @@
 class YellsController < ApplicationController
+  before_filter :setup
   def index
   end
 
   def show
+  end
+
+  private
+  def setup
+    @yell = Yell.find(params[:id]) || Yell.find_by_name(params[:id])
   end
 end
