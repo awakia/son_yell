8ec669803d780ca37e126b46f02fb6b72d05a9fd
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-11-17 10:53:51 +0900

    Execute rails g controller yells index show

---
+++ app/controllers/yells_controller.rb
@@ -0,0 +1,7 @@
+class YellsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SonYell::Application.routes.draw do
+  get "yells/index"
+
+  get "yells/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
